fa9132d2f7ac136fe692a796d8469532daada715
owner <struwwel4peter@gmail.com>
2023-08-23 15:04:12 +0200

    fix the Spring, because wasn't within package

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,3 +1,8 @@
+package guru.springframework.sfgpetclinic;
+
+
+
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
