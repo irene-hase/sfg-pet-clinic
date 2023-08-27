d7f708817542deabd5c57275d9764f83bc8565fb
owner <struwwel4peter@gmail.com>
2023-08-27 12:25:03 +0200

    Added Visit Entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,30 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "vets")
+public class Vet extends Person {
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+			inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+	private Set<Speciality> specialities = new HashSet<>();
+
+	public Set<Speciality> getSpecialities()
+	{
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities)
+	{
+		this.specialities = specialities;
+	}
 }
