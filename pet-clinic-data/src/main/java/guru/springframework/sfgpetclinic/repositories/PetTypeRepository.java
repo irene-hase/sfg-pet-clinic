a8573d20bc39c048a1670a0d03ecba2a9bdb9e25
owner <struwwel4peter@gmail.com>
2023-08-28 11:38:35 +0200

    added services for the entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
@@ -3,5 +3,5 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-interface PetTypeRepository extends CrudRepository<PetType, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
